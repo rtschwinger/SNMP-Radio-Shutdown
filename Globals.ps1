3faf775d8fda4ce4bcfdc4d98ee24cf8fa4b561d
bschwinger <rtsnhatl@gmail.com>
2012-05-08 17:37:40 -0400

    State when arrived at Seal Beach

--- Globals.ps1
+++ Globals.ps1
@@ -22,29 +22,36 @@ function Get-ScriptDirectory
 #Sample variable that provides the location of the script
 [string]$ScriptDirectory = Get-ScriptDirectory
 
-$settings = Import-Csv -Delimiter "," -Path c:\opt\Snmpset.ini
+$settings = Import-Csv -Delimiter "," -Path c:\opt\LEMSnmpset.ini
 $meshpoints = Import-Csv -Delimiter "," -Path c:\opt\LEMRadioList.csv
 
 $highestMesh = $settings.MeshE
 $AUTHKEY = $settings.authKey
 $PRIVKEY = $settings.privKey
-$YEAROID = $settings.yearOID
-$MONTHOID = $settings.monthOID
-$DAYOID = $settings.dayOID
-$HOUROID = $settings.hourOID
-$MINOID = $settings.minuteOID
-$SECOID = $settings.secondOID
-$LOTOID = $settings.lotOID
-$SCHEDOID = $settings.schedOID
 
-$sdTimeLength = 10
-$shutDownYR = 2012
-$shutDownMO = 1
-$shutDownDAY = 1
-$shutDownHOUR = 0
-$shutDownMIN = 0
-$shutDownSEC = 0
-$sdRadioIPAddr = "10.0.50.80"
+$shutdownDT = @(2012,1,1,1,1,0,10,2)
+$shutdownOIDs = @($settings.yearOID,$settings.monthOID,$settings.dayOID,$settings.hourOID,$settings.minuteOID,$settings.secondOID,$settings.lotOID,$settings.schedOID)
+$sdRadioIPAddrs = @{}
+
 $sdRadioSNMPV3User = "WMS"
 $sdSNMPV3AuthType = "SHA"
 $sdSNMPV3PrivType = "DES"
+$radioWait = 0
+$timePicked = ""
+$shutdownlenChosen = 1
+$dateTimeChosen = 1
+
+function RadioShutdown ($RadioIPs, $TimeAndDate, $LengthOfSD) {
+	# Try one or more commands
+	try {
+		foreach ($IPAddr in $RadioIPs) {
+			for ($i=0; $i -lt $shutdownDT.Count; $i++) {
+				& SnmpSet -addr $IPAddr -V 3 -u WMS -l authPriv -a SHA -A $AUTHKEY -p DES -P $PRIVKEY -O $shutdownOID[$i] -int $shutdownDT[$i]
+			}
+			Start-Sleep -s $radioWait
+		}
+	}
+	# Catch specific types of exceptions thrown by one of those commands
+	catch [System.IO.IOException] {
+	}
+}
